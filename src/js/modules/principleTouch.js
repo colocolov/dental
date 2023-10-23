a56756e219a0a55cc8fabcb071380c6b493314b9
colocolov <colocolov.stas@gmail.com>
2023-10-23 16:41:28 +0300

    upd some wish by Meyco

--- src/js/modules/principleTouch.js
+++ src/js/modules/principleTouch.js
@@ -1,30 +1,34 @@
-const principle = document.querySelectorAll(".principle__text");
+const principle = document.querySelectorAll(".principle__item");
 
 if (principle) {
+  
+  principle.forEach(el => {
+    el.addEventListener('click', function (e) {
+      el.children[2].children[0].classList.toggle('principle__text-active');
 
-principle.forEach(el => {
-  el.addEventListener('click', function (e) {
-    // узнаем тег при клике
-    const tagP = e.target.nodeName;
-
-    // если клик был по тексту то мы его пказываем
-    if (tagP == 'P') {
-      e.target.classList.toggle('principle__text-active');
-    }
-    
-    //если клик был родителю то ...
-    if (e.target.classList.contains('principle__text')) {
-      // если текста не показывается то показываем
-      if (!e.target.children[0].classList.contains('principle__text-active')) {
-        e.target.children[0].classList.add('principle__text-active');
-      }
-      // если текст показан, то скрываем его
-      else {
-        e.target.children[0].classList.remove('principle__text-active');
-      }
-    }
-
-  });
-});
-
+      // OLD
+      // // узнаем тег при клике
+      // const tagP = e.target.nodeName;
+      
+      // // если клик был по тексту то мы его пказываем
+      // if (tagP == 'P') {
+        //   e.target.classList.toggle('principle__text-active');
+        // }
+        
+        // //если клик был родителю то ...
+        // if (e.target.classList.contains('principle__text')) {
+          //   // если текста не показывается то показываем
+          //   if (!e.target.children[0].classList.contains('principle__text-active')) {
+            //     e.target.children[0].classList.add('principle__text-active');
+            //   }
+            //   // если текст показан, то скрываем его
+            //   else {
+              //     e.target.children[0].classList.remove('principle__text-active');
+              //   }
+              // }
+              /// END OLD  
+              
+    }); // end click
+  }); // end foreach
+          
 }
